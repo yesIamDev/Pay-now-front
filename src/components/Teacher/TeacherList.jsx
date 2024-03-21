8c451df88a0eccd5b708c66ea60356344708648e
unknown <davidkeukeu8@gmail.com>
2024-03-21 14:13:37 +0300

    add actions on the table

--- src/components/Teacher/TeacherList.jsx
+++ src/components/Teacher/TeacherList.jsx
@@ -1,62 +1,17 @@
 import React from "react";
 
 import { useRecoilValueLoadable, useRecoilState } from "recoil";
-
 import { teacherState, fetchTeachers } from "../../atoms/teachers";
-import { Button, Table } from "antd";
-import AddTeacher from "../../modals/AddTeacher";
 
-const Columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-    render: (name) => {
-      return <a className="font-bold">{name}</a>;
-    },
-  },
-  {
-    title: "lastName",
-    dataIndex: "lastname",
-    key: "lastname",
-    render: (lastname) => {
-      return <p className="font-bold">{lastname}</p>;
-    },
-  },
-  {
-    title: "Sex",
-    dataIndex: "sex",
-    key: "sex",
-  },
-  {
-    title: "Age",
-    dataIndex: "age",
-    key: "age",
-  },
-  {
-    title: "Salary",
-    dataIndex: "salary",
-    key: "salary",
-  },
-  {
-    title: "Actions",
-    dataIndex: "actions",
-    key: "actions",
-    render: () => {
-      <button className="border border-blue-600 text-blue-600 hover:text-white hover:bg-blue-600 py-1 px-4 rounded-lg">
-        Voir
-      </button>;
-    },
-  },
-];
+
+import { Table,Modal } from "antd";
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+
+import AddTeacher from "../../modals/AddTeacher";
 
 export default function TeacherList() {
   const teachersLoadable = useRecoilValueLoadable(fetchTeachers);
   const [teachers, setTeachers] = useRecoilState(teacherState);
-
-  const handleClick = function (teacherId) {
-    setTeachers((s) => ({ ...s, currentTeacher: teacherId }));
-  };
 
   if (teachersLoadable.state === "loading") {
     return <div>Chargement en cours...</div>;
@@ -69,7 +24,70 @@ export default function TeacherList() {
   }
 
   const teachersList = teachersLoadable.contents;
+
+  const handleClick =  (teacherId) => {
+    setTeachers((s) => ({ ...s, currentTeacher: teacherId }));
+  };
+
+  const handleDelet =  () => {
+      Modal.confirm({
+        title: 'Are you sure, you want to delete this teacher ?',
+        okText: 'Yes',
+        okType: 'danger',
+        onOk: () => {
+            console.log("Succes")
+        }
+      })
+  }
+
+  const Columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (name) => {
+        return <a className="font-bold">{name}</a>;
+      },
+    },
+    
+    {
+      title: "lastName",
+      dataIndex: "lastname",
+      key: "lastname",
+      render: (lastname) => {
+        return <p className="font-bold">{lastname}</p>;
+      },
+    },
+    {
+      title: "Sex",
+      dataIndex: "sex",
+      key: "sex",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Salary",
+      dataIndex: "salary",
+      key: "salary",
+    },
+    {
+      title: "Actions",
+      key: "actions",
+      render: (record) => {
+        return <>
+          <EditOutlined />
+          <DeleteOutlined onClick={() => {
+              handleDelet()
+          }}  style={{color: 'red', marginLeft:'12px'}} />
+        </>
+      },
+    },
+  ];
   
+
   return (
     <div className="container">
       <div>
